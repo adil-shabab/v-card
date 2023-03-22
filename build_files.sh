bf11de2b37fe81f12605dcc4222a39f82841fddd
adil-shabab <adilshabab2004@gmail.com>
2023-03-22 11:35:21 +0530

    som more chjange assed

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,6 @@
-
-echo " BUILD START"
+echo "BUILD START"
 
 python3.9 -m pip install -r requirements.txt
-python3.9 -m manage.py collectstatic --noinput --clear
-
+python3.9 manage collectstatic --noinput --clear
 
 echo "BUILD END"
