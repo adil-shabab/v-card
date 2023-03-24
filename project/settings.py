37f54a7275df75f5b5495431745293651aed6cf0
adil-shabab <adilshabab2004@gmail.com>
2023-03-24 12:14:26 +0530

    procfile deleted

--- project/settings.py
+++ project/settings.py
@@ -100,11 +100,23 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vcard',
+        'USER': 'adil',
+        'PASSWORD': 'HACKER@3197',
+        'HOST': 'localhost',
+        'PORT': '',
+     }
 }
 
 
